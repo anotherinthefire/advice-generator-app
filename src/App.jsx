da3297e052244179ce19a45e60415d2301900e24
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-14 20:56:34 +0800

    addons

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
       <div className='h-fix w-11/12 sm:w-11/12 md:w-2/3 lg:w-2/3 xl:w-1/3 mx-auto rounded-2xl bg-neutral-darkGrayishBlue flex flex-col justify-between'>
         <div className="text-center">
           {data && (
-            <div className="mt-10">
-              <p className="text-primary-neonGreen font-medium text-sm">ADVICE #{data.id}</p>
+            <div>
+              <p className="text-primary-neonGreen font-medium text-sm pt-10 pb-5">ADVICE #{data.id}</p>
               <p className="px-10 text-2xl font-bold text-primary-lightCyan">&quot;{data.advice}&quot;</p>
             </div>
           )}
@@ -30,7 +30,7 @@ function App() {
         <div className="relative px-10 py-10">
           <img src={kowt} />
         </div>
-        <div className="flex justify-center items-center bg-primary-neonGreen rounded-full w-16 h-16 mx-auto cursor-pointer -mb-8" onClick={fetchAdvice}>
+        <div className="flex justify-center items-center bg-primary-neonGreen rounded-full w-16 h-16 mx-auto cursor-pointer -mb-8 hover:shadow-[rgba(0,_191,102,_.8)_0px_0px_50px]" onClick={fetchAdvice}>
           <button><img src={dice} alt="dice" /></button>
         </div>
       </div>
